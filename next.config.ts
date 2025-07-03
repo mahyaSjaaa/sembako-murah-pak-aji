2ede78fac25ec6dc22cd46dd030b38916aaa524c
raffimahya <raffimlbb.23@gmail.com>
2025-07-03 23:19:08 +0700

    feat: add security headers (CSP, X-Frame, etc)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    const securityHeaders = [
+      {
+        key: "Content-Security-Policy",
+        value: "default-src 'self'; script-src 'self'; object-src 'none';",
+      },
+      {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+      },
+      {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+      },
+      {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+      },
+      {
+        key: "Permissions-Policy",
+        value: "geolocation=(), microphone=(), camera=()",
+      },
+    ];
+
+    return [
+      {
+        source: "/(.*)", // semua route
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
